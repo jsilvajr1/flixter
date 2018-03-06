d6ab3fe63a0b58e017814e91dc16bcaef02b6301
jorge silva <jsilvajr1@yahoo.com>
2018-03-06 11:22:53 -0500

    listing courses

---
+++ db/migrate/20180306153513_create_courses.rb
@@ -0,0 +1,12 @@
+class CreateCourses < ActiveRecord::Migration[5.0]
+  def change
+    create_table :courses do |t|
+      t.string :title
+      t.string :description
+      t.decimal :cost
+      t.integer :user_id
+      t.timestamps
+    end
+    add_index :course, :user_id
+  end
+end
