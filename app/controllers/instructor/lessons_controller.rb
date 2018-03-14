2cc2b70867e26659580b08deda9a7771e4c00160
jorge silva <jsilvajr1@yahoo.com>
2018-03-13 23:18:52 -0400

    lesson 21

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,2 +1,24 @@
 class Instructor::LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_section
+  
+  def new
+    @lesson = Lesson.new
+  end
+
+   def require_authorized_for_current_section
+    if current_section.course.user != current_user
+      return render plain: 'Unauthorized', status: :unauthorized
+    end
+  end
+
+  private
+  
+  helper_method :current_section
+  def current_section
+    @current_section ||= Section.find(params[:section_id])
+
+  def lesson_params
+    params.require(:lesson).permit(:title, :subtitle)
+  end
 end
--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,2 +1,4 @@
 class Lesson < ApplicationRecord
+  belongs_to :section
+  has_many :lessons
 end
