b33c581f8863ee086aea8cacff7b510ecd50262a
jorge silva <jsilvajr1@yahoo.com>
2018-03-22 20:15:17 -0400

    video bug fixed

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,24 +1,24 @@
 class Instructor::LessonsController < ApplicationController
   before_action :authenticate_user!
   before_action :require_authorized_for_current_section
-  
+
   def new
     @lesson = Lesson.new
   end
-  
+
   def create
     @lesson = current_section.lessons.create(lesson_params)
     redirect_to instructor_course_path(current_section.course)
   end
 
-   def require_authorized_for_current_section
+  private
+
+  def require_authorized_for_current_section
     if current_section.course.user != current_user
-      return render plain: 'Unauthorized', status: :unauthorized
+      render plain: 'Unauthorized', status: :unauthorized
     end
   end
 
-  private
-  
   helper_method :current_section
   def current_section
     @current_section ||= Section.find(params[:section_id])
